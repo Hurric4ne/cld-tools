afa22a7909dffb1cee3ebdf99741de8b12045295
Hurric4ne <scholl.s.tobias@web.de>
2025-01-09 14:22:12 +0100

    UPDATE CargoMission Vue-Comoonent

--- src/views/CargoMissionView.vue
+++ src/views/CargoMissionView.vue
@@ -1,83 +1,59 @@
 <template>
-  <div class="cargo-list">
-    <!-- Title -->
-    <h1>Cargo-Mission List</h1>
-
+  <div class="cargo-mission-list">
+    <h1>Cargo Mission List</h1>
     <div class="filters">
-      <label for="reputationFilter">Reputation Requirement:</label>
-      <select id="reputationFilter" v-model="filters.reputation">
+      <label for="reputationRankFilter">Reputation Rank:</label>
+      <select id="reputationRankFilter" v-model="filters.reputationRank">
         <option value="">All</option>
-        <option
-          v-for="(displayInfo, key) in rankDisplayInfos"
-          :key="key"
-          :value="key"
-        >
-          {{ displayInfo[0] }}
-        </option>
+        <option v-for="rank in reputationRanks" :key="rank" :value="rank">{{ rank }}</option>
       </select>
 
-      <label for="rewardFilter">Mission Reward:</label>
-      <input
-        type="number"
-        id="rewardFilter"
-        v-model.number="filters.reward"
-        placeholder="Enter reward value"
-      />
-
-      <label for="cargoAmountFilter">Cargo Amount:</label>
-      <select id="cargoAmountFilter" v-model="filters.cargoAmount">
+      <label for="maxSCUSizeFilter">Max SCU Size:</label>
+      <select id="maxSCUSizeFilter" v-model="filters.maxSCUSize">
         <option value="">All</option>
-        <option v-for="option in cargoAmountOptions" :key="option" :value="option">
-          {{ option }}
-        </option>
+        <option v-for="size in maxSCUSizes" :key="size" :value="size">{{ size }}</option>
       </select>
 
       <label for="cargoRouteFilter">Cargo Route:</label>
       <select id="cargoRouteFilter" v-model="filters.cargoRoute">
         <option value="">All</option>
-        <option v-for="option in cargoRouteOptions" :key="option" :value="option">
-          {{ option }}
-        </option>
+        <option v-for="route in cargoRoutes" :key="route" :value="route">{{ route }}</option>
       </select>
 
-      <label for="maxSCUSizeFilter">Max Container Size (SCU):</label>
-      <select id="maxSCUSizeFilter" v-model="filters.maxSCUSize">
+      <label for="cargoGradeFilter">Cargo Grade:</label>
+      <select id="cargoGradeFilter" v-model="filters.cargoGrade">
         <option value="">All</option>
-        <option v-for="size in sortedMaxSCUSizes" :key="size" :value="size">
-          {{ size }}
-        </option>
+        <option v-for="grade in cargoGrades" :key="grade" :value="grade">{{ grade }}</option>
       </select>
+
+      <label for="locationFilter">Location:</label>
+      <select id="locationFilter" v-model="filters.location">
+        <option value="">All</option>
+        <option v-for="location in locations" :key="location" :value="location">{{ location }}</option>
+      </select>
+
+      <label for="rewardFilter">Reward:</label>
+      <input type="number" id="rewardFilter" v-model.number="filters.reward" placeholder="Enter reward value" />
+
+      <label for="directRouteFilter">Only direct Routes:</label>
+      <input type="checkbox" id="directRouteFilter" v-model="filters.isDirectRoute" />
     </div>
 
+    <h2>{{ filteredMissions.length }} items found</h2>
+
     <div class="mission-list">
-      <div
-        v-for="mission in filteredMissions"
-        :key="mission.missionKey"
-        class="mission-item"
-      >
-        <h3>{{ mission.missionKey }}</h3>
-        <p><strong>Reward:</strong> {{ mission.missionReward ? mission.missionReward.toLocaleString() : "N/A" }} aUEC</p>
-        <p><strong>Rank:</strong> {{ rankDisplayInfos[mission.reputationRequirements][0] }}</p>
-        <p><strong>Max SCU Size:</strong> {{ mission.maxSCUSize }}</p>
-        <p><strong>Cargo Amount:</strong> {{ mission.cargoAmount }}</p>
-        <p><strong>Cargo Route:</strong> {{ mission.cargoRoute }}</p>
-        <p>
-          <strong>Deadline:</strong>
-          {{ mission.missionDeadline.slice(0, -2) }} minutes
-        </p>
-        <p>
-          <strong>Failure:</strong>
-          {{ mission.missionResultReputationRewards["outcome 2"] ? mission.missionResultReputationRewards["outcome 2"].toLocaleString() : 'N/A' }} Reputation
-        </p>
-        <p>
-          <strong>Abandon:</strong>
-          {{ mission.missionResultReputationRewards["outcome 3"] ? mission.missionResultReputationRewards["outcome 3"].toLocaleString() : 'N/A' }} Reputation
-        </p>
-        <p class="success">
-          <strong>Success:</strong>
-          {{ mission.missionResultReputationRewards["outcome 1"] ? mission.missionResultReputationRewards["outcome 1"].toLocaleString() : 'N/A' }} Reputation
-          ({{ rankDisplayInfos[mission.reputationRequirements][1] }} to the next higher Rank)
-        </p>
+      <div v-for="mission in filteredMissions" :key="mission.internalName" class="mission-item">
+        <h3>{{ mission.displayTitle }}</h3>
+        <p><strong>Reward:</strong> {{ mission.reward.toLocaleString() }}</p>
+        <p><strong>Cargo Amount:</strong> {{ mission.cargoAmount }} (max. {{ mission.maxSCUSize }} Boxes)</p>
+        <p><strong>Location:</strong> {{ mission.location }}</p>
+        <p><strong>Reputation Rank:</strong> {{ mission.reputationRank }}</p>
+        <p><strong>Reputation:</strong></p>
+        <ul class="reputation-list">
+          <li v-for="rep in mission.reputation" :key="Object.keys(rep)[0]">
+            {{ Object.keys(rep)[0] }}: {{ Object.values(rep)[0].toLocaleString() }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -85,114 +61,86 @@
 
 <script>
 import { reactive, computed } from "vue";
-import cargoData from "@/assets/json/finalAggregatedData.json";
+import cargoMissions from "@/assets/json/cargo_missions.json";
 
 export default {
-  name: "CargoMissions",
+  name: "CargoMissionView",
   setup() {
     const filters = reactive({
-      reputation: "",
-      reward: 0,
-      cargoAmount: "",
-      cargoRoute: "",
+      reputationRank: "",
       maxSCUSize: "",
+      cargoRoute: "",
+      cargoGrade: "",
+      location: "",
+      reward: 0,
+      isDirectRoute: false,
     });
 
-    const rankDisplayInfos = {
-      Delivery_Rank0: ["Trainee (0)", "100%"],
-      Delivery_Rank1: ["Rookie (1)", "25%"],
-      Delivery_Rank2: ["Junior (2)", "10%"],
-      Delivery_Rank3: ["Member (3)", "~4.5"],
-      Delivery_Rank4: ["Experienced (4)", "4"],
-      Delivery_Rank5: ["Senior (5)", "2.5%"],
-      Delivery_Rank6: ["Master (6)", "6.15%"],
-    };
-
-    const cargoAmountOptions = ["ExtraSmall", "Small", "Supply", "Bulk"];
-    const cargoRouteOptions = ["Local", "Planetary", "Solar", "Interstellar"];
-
-    const sortedMaxSCUSizes = computed(() => {
-      const uniqueSizes = new Set(cargoData.map((m) => m.maxSCUSize));
-      let sizes = [...uniqueSizes].filter((size) => size !== "32SCU");
-      sizes.push("32SCU");
-      return sizes;
-    });
+    const reputationRanks = ["Trainee", "Rookie", "Junior", "Member", "Experienced", "Senior", "Master"];
+    const maxSCUSizes = ["1 SCU", "4 SCU", "8 SCU", "16 SCU", "32 SCU"];
+    const cargoRoutes = ["Local", "Planetary", "Solar", "Interstellar"];
+    const cargoGrades = ["Extra Small", "Small", "Medium", "Large"];
+    const locations = [...new Set(cargoMissions.map(mission => mission.location))];
 
     const filteredMissions = computed(() => {
-      return cargoData.filter((mission) => {
-        const matchReputation =
-          !filters.reputation || mission.reputationRequirements === filters.reputation;
-        const matchReward = !filters.reward || mission.missionReward >= filters.reward;
-        const matchCargoAmount =
-          !filters.cargoAmount || mission.cargoAmount === filters.cargoAmount;
-        const matchCargoRoute =
-          !filters.cargoRoute || mission.cargoRoute === filters.cargoRoute;
-        const matchMaxSCUSize =
-          !filters.maxSCUSize || mission.maxSCUSize === filters.maxSCUSize;
+      return cargoMissions.filter(mission => {
+        const matchReputationRank = !filters.reputationRank || mission.reputationRank === filters.reputationRank;
+        const matchMaxSCUSize = !filters.maxSCUSize || mission.maxSCUSize === filters.maxSCUSize;
+        const matchCargoRoute = !filters.cargoRoute || mission.cargoRoute === filters.cargoRoute;
+        const matchCargoGrade = !filters.cargoGrade || mission.cargoGrade === filters.cargoGrade;
+        const matchLocation = !filters.location || mission.location === filters.location;
+        const matchReward = !filters.reward || mission.reward >= filters.reward;
+        const matchDirectRoute = !filters.isDirectRoute || mission.isDirectRoute === filters.isDirectRoute;
 
-        return (
-          matchReputation &&
-          matchReward &&
-          matchCargoAmount &&
-          matchCargoRoute &&
-          matchMaxSCUSize
-        );
+        return matchReputationRank && matchMaxSCUSize && matchCargoRoute && matchCargoGrade && matchLocation && matchReward && matchDirectRoute;
       });
     });
 
     return {
-      cargoData,
       filters,
-      rankDisplayInfos,
-      cargoAmountOptions,
-      cargoRouteOptions,
-      sortedMaxSCUSizes,
+      reputationRanks,
+      maxSCUSizes,
+      cargoRoutes,
+      cargoGrades,
+      locations,
       filteredMissions,
     };
   },
 };
 </script>
 
-<style scoped lang="scss">
-.cargo-list {
+<style scoped>
+.cargo-mission-list {
   width: 100%;
-  max-width: 840px;
-  margin: 40px auto;
-  border: 1px solid var(--color-red);
-  padding: 20px;
+  max-width: 1000px;
+  margin: 20px auto;
+  padding: 10px;
 
-  h1 {
-    color: var(--color-white);
-    font-weight: bold;
+  h1,h2 {
     text-align: center;
-    margin-top: 0;
-    margin-bottom: 25px;
   }
+}
 
-  .filters {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    gap: 10px 50px;
-    text-align: right;
-    margin: 0 50px 50px 0;
-  }
+.filters {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 10px 20px;
+  margin-bottom: 20px;
+}
 
-  .mission-item {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    row-gap: 10px;
-    border: 1px solid var(--color-lightgray);
-    margin-bottom: 20px;
-    padding: 20px;
+.mission-list {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 20px;
+}
 
-    .success,
-    h3 {
-      grid-column: 1/span 2;
-    }
+.mission-item {
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+}
 
-    p {
-      margin: 0;
-    }
-  }
+.reputation-list li {
+  margin: 7px 0;
 }
 </style>
